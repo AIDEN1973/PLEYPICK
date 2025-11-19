36f2a9f9c7aa9274f462443d4a7b06f335146ce9
Your Name <your.email@example.com>
2025-11-19 10:19:34 +0900

    부품 색상 저장 정상(10)

--- src/composables/useSupabase.js
+++ src/composables/useSupabase.js
@@ -10,6 +10,13 @@ const supabaseOptions = {
   auth: {
     persistSession: true,
     autoRefreshToken: true
+  },
+  global: {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+    }
   }
 }
 
